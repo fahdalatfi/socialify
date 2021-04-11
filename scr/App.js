9f3bbe690f11a02b4f25063c31be9b0c830ea435
fahdalatfi <alatfifahd2@gmail.com>
2021-04-11 21:19:18 +0300

    changes

--- scr/App.js
+++ scr/App.js
@@ -1,2 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+
+
+const App = () => {
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="868cb135-7826-4476-9c6f-aa3f87e0eadd"
+        userName="Alatfifahd"
+        userSecret="123qweasd"
+
+
+        />
+    );
+}
+
+export default App
